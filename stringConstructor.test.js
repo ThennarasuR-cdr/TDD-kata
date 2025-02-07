0575e0fb0185bc8f9b4ab08b7cf380875d867f7c
Thennarasu <thennarasu4499@gmail.com>
2025-02-07 09:22:23 +0530

    Refactor function name as it supports multiple operations

--- stringConstructor.test.js
+++ stringConstructor.test.js
@@ -1,10 +1,10 @@
-const add = require('./stringConstructor.js');
+const calculate = require('./stringConstructor.js');
 
 describe('string constructor', () => {
     it('should return 0 when empty string is constructed', () => {
         const string = '';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(0);
     });
@@ -12,7 +12,7 @@ describe('string constructor', () => {
     it('should return the value when a string of length 1 is constructed', () => {
         const string = '2';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(2);
     });
@@ -20,7 +20,7 @@ describe('string constructor', () => {
     it('should return the value when a string with two numbers is constructed', () => {
         const string = '2,3';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(5);
     });
@@ -28,7 +28,7 @@ describe('string constructor', () => {
     it('should return the value when a string with multiple numbers is constructed', () => {
         const string = '2,3,4,5';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(14);
     });
@@ -36,7 +36,7 @@ describe('string constructor', () => {
     it('should return the value when a string with two digit numbers is constructed', () => {
         const string = '12,13';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(25);
     });
@@ -44,7 +44,7 @@ describe('string constructor', () => {
     it('should return the value when a string with numbers on new line is constructed', () => {
         const string = '12,1\n3';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(16);
     });
@@ -52,27 +52,27 @@ describe('string constructor', () => {
     it('should return the value when a string with custom delimitters is constructed', () => {
         const string = '//;\n12;1;3';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(16);
     });
 
     it('should throw exception when negative numbers are passed', () => {
         const string = '//;\n12;1;-3';
-        expect(()=>add(string))
+        expect(() => calculate(string))
             .toThrow("negative numbers not allowed -3");
     });
 
     it('should throw exception with all neagtive numbers in message when negative numbers are passed', () => {
         const string = '//;\n12;1;-3;5;-9;-4';
-        expect(()=>add(string))
+        expect(() => calculate(string))
             .toThrow("negative numbers not allowed -3,-9,-4");
     });
 
     it('should multiply the numbers when the custom delimitter is *', ()=>{
         const string='//*\n12*3*2';
 
-        const result = add(string);
+        const result = calculate(string);
 
         expect(result).toBe(72);
     })
